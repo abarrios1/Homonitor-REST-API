7f9f6346b46ce3bd6fd53df13ead8cbeaeb33ebf
Anthony Barrios <anthony.barrios@wowinc.com>
2020-06-22 22:55:31 -0500

    Add CQL lirary for reading from Cassandra

--- controllers/devices.go
+++ controllers/devices.go
@@ -2,28 +2,46 @@ package controllers
 
 import (
 	"bytes"
+	"github.com/abarrios1/HomonitorBackend/lib"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/abarrios1/HomonitorBackend/models"
+	"github.com/gocql/gocql"
 	"github.com/labstack/echo/v4"
 )
 
-// GetDevice ...
+// GetDevice ... Collect individual device information
 func GetDevice(ctx echo.Context) error {
+	cqlHost := os.Getenv("CASSANDRA_HOST")
+	cluster := gocql.NewCluster(cqlHost)
+	cluster.Keyspace = "Homonitor"
+	cluster.Consistency = gocql.Quorum
+	session, _ := cluster.CreateSession()
+	defer session.Close()
 
+	session.Query(`SELECT * FROM device WHERE mac_address = ?`, "ValueInsertFromCtx").Iter()
 
 	return ctx.JSON(http.StatusOK, "")
 }
 
-// GetAllDevices ...
+// GetAllDevices ... Collect all devices information
 func GetAllDevices(ctx echo.Context) error {
+	cqlHost := os.Getenv("CASSANDRA_HOST")
+	cluster := gocql.NewCluster(cqlHost)
+	cluster.Keyspace = "Homonitor"
+	cluster.Consistency = gocql.Quorum
+	session, _ := cluster.CreateSession()
+	defer session.Close()
 
+	session.Query(`SELECT * FROM device`).Iter()
 
 	return ctx.JSON(http.StatusOK, "")
 }
 
-// CreateDevice ...
+// CreateDevice ... Create a new device and insert into C*
 func CreateDevice(ctx echo.Context) error {
 	device := new(models.Device)
 
@@ -37,11 +55,18 @@ func CreateDevice(ctx echo.Context) error {
 
 	ctx.Request().Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 	er := ctx.Bind(device)
-
 	if er != nil {
 		panic(er)
 	}
 
+	session, err := lib.ConnectCassandraCluster()
+	defer session.Close()
+
+	// insert a tweet
+	if err := session.Query(`INSERT INTO device (hostname, device_type, ip_address, serial_number, mac_address) VALUES (?, ?, ?, ?, ?)`,
+		"", "", "", "", "").Exec(); err != nil {
+		log.Fatal(err)
+	}
 
 	return ctx.JSON(http.StatusOK, device)
 }
@@ -49,21 +74,18 @@ func CreateDevice(ctx echo.Context) error {
 // UpdateDevice ...
 func UpdateDevice(ctx echo.Context) error {
 
-
 	return ctx.JSON(http.StatusOK, "")
 }
 
 // DeleteDevice ...
 func DeleteDevice(ctx echo.Context) error {
 
-
 	return ctx.JSON(http.StatusOK, "")
 }
 
 // DeviceInterfaceEntryStats ...
-func DeviceInterfaceEntryStats (ctx echo.Context) error {
+func DeviceInterfaceEntryStats(ctx echo.Context) error {
 	var deviceInterfaceEntryStats models.DeviceInterfaceEntryStats
-
 
 	return ctx.JSON(http.StatusOK, deviceInterfaceEntryStats)
 }
